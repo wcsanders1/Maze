affd4f7bb31a2e34dfa37cc5708fc670b5b9bd44
William Sanders <william.sanders.dev@gmail.com>
2016-07-31 14:22:10 -0400

    draws a square, making progress

--- Maze/Form1.Designer.cs
+++ Maze/Form1.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.control_panel = new System.Windows.Forms.Panel();
-            this.canvas = new System.Windows.Forms.Panel();
-            this.num_lines = new System.Windows.Forms.Label();
-            this.angle = new System.Windows.Forms.Label();
-            this.length = new System.Windows.Forms.Label();
-            this.increment_label = new System.Windows.Forms.Label();
-            this.num_lines_txt = new System.Windows.Forms.TextBox();
-            this.angle_txt = new System.Windows.Forms.TextBox();
-            this.length_txt = new System.Windows.Forms.TextBox();
-            this.increment_txt = new System.Windows.Forms.TextBox();
             this.go_button = new System.Windows.Forms.Button();
+            this.increment_txt = new System.Windows.Forms.TextBox();
+            this.length_txt = new System.Windows.Forms.TextBox();
+            this.angle_txt = new System.Windows.Forms.TextBox();
+            this.num_lines_txt = new System.Windows.Forms.TextBox();
+            this.increment_label = new System.Windows.Forms.Label();
+            this.length = new System.Windows.Forms.Label();
+            this.angle = new System.Windows.Forms.Label();
+            this.num_lines = new System.Windows.Forms.Label();
+            this.canvas = new System.Windows.Forms.Panel();
             this.control_panel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,52 +57,51 @@
             this.control_panel.Dock = System.Windows.Forms.DockStyle.Top;
             this.control_panel.Location = new System.Drawing.Point(0, 0);
             this.control_panel.Name = "control_panel";
-            this.control_panel.Size = new System.Drawing.Size(1097, 100);
+            this.control_panel.Size = new System.Drawing.Size(1113, 100);
             this.control_panel.TabIndex = 0;
             this.control_panel.Paint += new System.Windows.Forms.PaintEventHandler(this.control_panel_Paint);
             // 
-            // canvas
+            // go_button
             // 
-            this.canvas.BackColor = System.Drawing.SystemColors.Info;
-            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.canvas.Location = new System.Drawing.Point(0, 100);
-            this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(1097, 414);
-            this.canvas.TabIndex = 1;
-            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.go_button.Location = new System.Drawing.Point(928, 27);
+            this.go_button.Name = "go_button";
+            this.go_button.Size = new System.Drawing.Size(87, 39);
+            this.go_button.TabIndex = 8;
+            this.go_button.Text = "Go";
+            this.go_button.UseVisualStyleBackColor = true;
+            this.go_button.Click += new System.EventHandler(this.go_button_Click);
             // 
-            // num_lines
+            // increment_txt
             // 
-            this.num_lines.AutoSize = true;
-            this.num_lines.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.num_lines.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.num_lines.Location = new System.Drawing.Point(12, 31);
-            this.num_lines.Name = "num_lines";
-            this.num_lines.Size = new System.Drawing.Size(154, 25);
-            this.num_lines.TabIndex = 0;
-            this.num_lines.Text = "Number of Lines";
+            this.increment_txt.Location = new System.Drawing.Point(771, 31);
+            this.increment_txt.Name = "increment_txt";
+            this.increment_txt.Size = new System.Drawing.Size(45, 22);
+            this.increment_txt.TabIndex = 7;
+            this.increment_txt.Text = "1";
             // 
-            // angle
+            // length_txt
             // 
-            this.angle.AutoSize = true;
-            this.angle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.angle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.angle.Location = new System.Drawing.Point(237, 31);
-            this.angle.Name = "angle";
-            this.angle.Size = new System.Drawing.Size(63, 25);
-            this.angle.TabIndex = 1;
-            this.angle.Text = "Angle";
+            this.length_txt.Location = new System.Drawing.Point(501, 31);
+            this.length_txt.Name = "length_txt";
+            this.length_txt.Size = new System.Drawing.Size(45, 22);
+            this.length_txt.TabIndex = 6;
+            this.length_txt.Text = "5";
             // 
-            // length
+            // angle_txt
             // 
-            this.length.AutoSize = true;
-            this.length.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.length.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.length.Location = new System.Drawing.Point(423, 31);
-            this.length.Name = "length";
-            this.length.Size = new System.Drawing.Size(72, 25);
-            this.length.TabIndex = 2;
-            this.length.Text = "Length";
+            this.angle_txt.Location = new System.Drawing.Point(306, 31);
+            this.angle_txt.Name = "angle_txt";
+            this.angle_txt.Size = new System.Drawing.Size(45, 22);
+            this.angle_txt.TabIndex = 5;
+            this.angle_txt.Text = "74";
+            // 
+            // num_lines_txt
+            // 
+            this.num_lines_txt.Location = new System.Drawing.Point(170, 31);
+            this.num_lines_txt.Name = "num_lines_txt";
+            this.num_lines_txt.Size = new System.Drawing.Size(45, 22);
+            this.num_lines_txt.TabIndex = 4;
+            this.num_lines_txt.Text = "100";
             // 
             // increment_label
             // 
@@ -115,53 +114,54 @@
             this.increment_label.TabIndex = 3;
             this.increment_label.Text = "Increment";
             // 
-            // num_lines_txt
+            // length
             // 
-            this.num_lines_txt.Location = new System.Drawing.Point(170, 31);
-            this.num_lines_txt.Name = "num_lines_txt";
-            this.num_lines_txt.Size = new System.Drawing.Size(45, 22);
-            this.num_lines_txt.TabIndex = 4;
-            this.num_lines_txt.Text = "100";
+            this.length.AutoSize = true;
+            this.length.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.length.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.length.Location = new System.Drawing.Point(423, 31);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(72, 25);
+            this.length.TabIndex = 2;
+            this.length.Text = "Length";
             // 
-            // angle_txt
+            // angle
             // 
-            this.angle_txt.Location = new System.Drawing.Point(306, 31);
-            this.angle_txt.Name = "angle_txt";
-            this.angle_txt.Size = new System.Drawing.Size(45, 22);
-            this.angle_txt.TabIndex = 5;
-            this.angle_txt.Text = "74";
+            this.angle.AutoSize = true;
+            this.angle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.angle.Location = new System.Drawing.Point(237, 31);
+            this.angle.Name = "angle";
+            this.angle.Size = new System.Drawing.Size(63, 25);
+            this.angle.TabIndex = 1;
+            this.angle.Text = "Angle";
             // 
-            // length_txt
+            // num_lines
             // 
-            this.length_txt.Location = new System.Drawing.Point(501, 31);
-            this.length_txt.Name = "length_txt";
-            this.length_txt.Size = new System.Drawing.Size(45, 22);
-            this.length_txt.TabIndex = 6;
-            this.length_txt.Text = "5";
+            this.num_lines.AutoSize = true;
+            this.num_lines.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num_lines.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.num_lines.Location = new System.Drawing.Point(12, 31);
+            this.num_lines.Name = "num_lines";
+            this.num_lines.Size = new System.Drawing.Size(154, 25);
+            this.num_lines.TabIndex = 0;
+            this.num_lines.Text = "Number of Lines";
             // 
-            // increment_txt
+            // canvas
             // 
-            this.increment_txt.Location = new System.Drawing.Point(771, 31);
-            this.increment_txt.Name = "increment_txt";
-            this.increment_txt.Size = new System.Drawing.Size(45, 22);
-            this.increment_txt.TabIndex = 7;
-            this.increment_txt.Text = "1";
-            // 
-            // go_button
-            // 
-            this.go_button.Location = new System.Drawing.Point(928, 27);
-            this.go_button.Name = "go_button";
-            this.go_button.Size = new System.Drawing.Size(87, 39);
-            this.go_button.TabIndex = 8;
-            this.go_button.Text = "Go";
-            this.go_button.UseVisualStyleBackColor = true;
-            this.go_button.Click += new System.EventHandler(this.go_button_Click);
+            this.canvas.BackColor = System.Drawing.SystemColors.Info;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 100);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1113, 443);
+            this.canvas.TabIndex = 1;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
             // 
             // Maze
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1097, 514);
+            this.ClientSize = new System.Drawing.Size(1113, 543);
             this.Controls.Add(this.canvas);
             this.Controls.Add(this.control_panel);
             this.Name = "Maze";
---
+++ Maze/MazePlatform.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze
+{
+    public class MazePlatform
+    {
+        public static int[] mazeX = new int[200];
+        public static int[] mazeY = new int[200];
+        public static string[,] status = new string[200, 200];
+
+        public static void CreateMazePlatform()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                mazeX[i] = i + 20;
+            }
+            for (int i = 0; i < 200; i++)
+            {
+                mazeY[i] = i + 20;
+            }
+
+            InitializeMazeStatus();
+
+            CreateMazeBorders();
+        }
+
+        public static void InitializeMazeStatus()
+        {
+            for (int x = 0; x < 200; x++)
+            {
+                for (int y = 0; y < 200; y++)
+                {
+                    status[x, y] = "open";
+                }
+            }
+        }
+
+        public static void CreateMazeBorders()
+        {
+            for (int x = 0; x < 200; x++)
+            {
+                status[x, 0] = "closed";    //closes bottom border
+                status[x, 199] = "closed";  //closes top border
+            }
+            for (int y = 0; y < 200; y++)
+            {
+                status[0, y] = "closed";    //closes left border
+                status[199, y] = "closed";  //closes right border
+            }
+        }
+    }
+}
